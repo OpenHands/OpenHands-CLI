968a7d901b2bfd0824dccbcc172b5256c4904e70
Rohit Malhotra <rohitvinodmalhotra@gmail.com>
2026-01-02 15:26:50 -0500

    Update __init__.py

--- openhands_cli/acp_impl/agent/__init__.py
+++ openhands_cli/acp_impl/agent/__init__.py
@@ -1,16 +1,10 @@
-from openhands.sdk import Conversation
 from openhands_cli.acp_impl.agent.launcher import run_acp_server
 from openhands_cli.acp_impl.agent.local_agent import LocalOpenHandsACPAgent
 from openhands_cli.acp_impl.agent.remote_agent import OpenHandsCloudACPAgent
-from openhands_cli.acp_impl.events.event import EventSubscriber
-from openhands_cli.setup import load_agent_specs
 
 
 __all__ = [
     "run_acp_server",
     "OpenHandsCloudACPAgent",
     "LocalOpenHandsACPAgent",
-    "load_agent_specs",
-    "Conversation",
-    "EventSubscriber",
 ]
